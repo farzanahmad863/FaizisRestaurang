3a0eeb14ff1b86d3644d7a8ef3321aa25375e848
farzanahmad863 <farzan.ahmad863@yahoo.com>
2021-02-26 17:05:49 +0100

    Added Category model

--- FaizisRestaurang/Data/ApplicationDbContext.cs
+++ FaizisRestaurang/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FaizisRestaurang.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace FaizisRestaurang.Data
             : base(options)
         {
         }
+        public DbSet<Category> Category { get; set; }
     }
 }
