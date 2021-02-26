6b038a4fc82978bc8a18b94aa1645fb225a9d958
farzanahmad863 <farzan.ahmad863@yahoo.com>
2021-02-26 13:46:15 +0100

    Added Areas of Admin and Customer

--- FaizisRestaurang/Areas/Customer/Controllers/HomeController.cs
+++ FaizisRestaurang/Areas/Customer/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace FaizisRestaurang.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- FaizisRestaurang/Startup.cs
+++ FaizisRestaurang/Startup.cs
@@ -62,7 +62,7 @@ namespace FaizisRestaurang
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
