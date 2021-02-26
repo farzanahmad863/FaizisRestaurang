2f882c9222c3d6b7982676c35591953cd370fa10
farzanahmad863 <farzan.ahmad863@yahoo.com>
2021-02-26 11:47:15 +0100

    Add project files.

---
+++ FaizisRestaurang/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FaizisRestaurang.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
