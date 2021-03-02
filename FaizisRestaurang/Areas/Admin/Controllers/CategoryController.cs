8ab0856c309d0c5c4e83355596407cdd5dc83395
farzanahmad863 <farzan.ahmad863@yahoo.com>
2021-03-02 10:49:13 +0100

    Added UI for the category page

---
+++ FaizisRestaurang/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using FaizisRestaurang.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaizisRestaurang.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //Get
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Category.ToListAsync());
+        }
+    }
+}
